dc2ff4f48aef5ec23202711ace614a24e36d46d7
Locoyoccim <charlyhernandez917@gmail.com>
2025-08-09 19:38:22 -0600

    refactor: mejorar la estructura y legibilidad del componente About y App

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,77 +6,77 @@ import { motion } from "framer-motion";
 import { useRef } from "react";
 
 function About() {
-  const constraintsRef = useRef(null);
-  const { ref, replay } = useScramble({
-    text: "Carlos Antonio Hernandez robles",
-    speed: 0.45,
-    tick: 1,
-    overflow: true,
-  });
-  return (
-    <>
-      <section id="sobre_mi">
-        <div className="performance">
-          <h1 className="name" ref={ref} onMouseOver={replay}></h1>
-          <p className="contact_details">
-            carlos_360@outlook.es | 374 107 7582 | Tequila, Jal, Mexico
-          </p>
-          <div className="social">
-            <a href="https://github.com/Locoyoccim" target="_blank">
-              <i className="bi bi-github"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/carlos-antonio-hernandez-robles-1990021a4/"
-              target="_blank"
-            >
-              <i className="bi bi-linkedin"></i>
-            </a>
-            <a href="/Hoja_de_Vida_Carlos.pdf" target="_blank">
-              <i className="bi bi-file-earmark-person-fill"></i>
-            </a>
-            <a
-              href="https://www.frontendmentor.io/profile/Locoyoccim"
-              target="_blank"
-            >
-              <img
-                src={mentor}
-                alt="FrontEndMentor"
-                className="frontEndMentor"
-              />
-            </a>
-          </div>
-        </div>
-        <motion.div ref={constraintsRef} className="img_performance">
-          <motion.img
-            initial={{ scale: 0.8, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }} 
-            transition={{
-              duration: 0.6,
-              ease: "easeInOut" 
-            }}
-            whileHover={{ scale: 1.05 }}
-            drag
-            dragConstraints={constraintsRef}
-            className="male_computer"
-            src="/developer_male.jpg"
-            alt="developer"
-          />
-        </motion.div>
-      </section>
-      <div className="description">
-        <p>
-          Soy un desarrollador con experiencia en tecnologías front-end y
-          back-end, especializado en <span className="blue_text">ReactJS</span>,
-          <span className="blue_text"> HTML5</span>,
-          <span className="blue_text"> CSS</span> y
-          <span className="blue_text"> JavaScript.</span> He trabajado en roles
-          de coordinación y atención al cliente, lo cual me ha permitido
-          desarrollar mis habilidades en solución de problemas, gestión de
-          equipos y comunicación efectiva.
-        </p>
-      </div>
-    </>
-  );
+    const constraintsRef = useRef(null);
+    const { ref, replay } = useScramble({
+        text: "Carlos Antonio Hernandez robles",
+        speed: 0.45,
+        tick: 1,
+        overflow: true,
+    });
+    return (
+        <>
+            <section id="sobre_mi">
+                <div className="performance">
+                    <h1 className="name" ref={ref} onMouseOver={replay}></h1>
+                    <p className="contact_details">
+                        carlos_360@outlook.es | 374 107 7582 | Tequila, Jal, Mexico
+                    </p>
+                    <div className="social">
+                        <a href="https://github.com/Locoyoccim" target="_blank">
+                            <i className="bi bi-github"></i>
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/carlos-antonio-hernandez-robles-1990021a4/"
+                            target="_blank"
+                        >
+                            <i className="bi bi-linkedin"></i>
+                        </a>
+                        <a href="/Hoja_de_Vida_Carlos.pdf" target="_blank">
+                            <i className="bi bi-file-earmark-person-fill"></i>
+                        </a>
+                        <a
+                            href="https://www.frontendmentor.io/profile/Locoyoccim"
+                            target="_blank"
+                        >
+                            <img
+                                src={mentor}
+                                alt="FrontEndMentor"
+                                className="frontEndMentor"
+                            />
+                        </a>
+                    </div>
+                </div>
+                <motion.div ref={constraintsRef} className="img_performance">
+                    <motion.img
+                        initial={{ scale: 0.8, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        transition={{
+                            duration: 0.6,
+                            ease: "easeInOut",
+                        }}
+                        whileHover={{ scale: 1.05 }}
+                        drag
+                        dragConstraints={constraintsRef}
+                        className="male_computer"
+                        src="/developer_male.jpg"
+                        alt="developer"
+                    />
+                </motion.div>
+            </section>
+            <div className="description">
+                <p>
+                    Soy un desarrollador con experiencia en tecnologías front-end y
+                    back-end, especializado en <span className="blue_text">ReactJS</span>,
+                    <span className="blue_text"> HTML5</span>,
+                    <span className="blue_text"> CSS</span> y
+                    <span className="blue_text"> JavaScript.</span> He desempeñado roles
+                    de coordinación y atención al cliente, lo que me ha permitido
+                    fortalecer habilidades clave como la solución de problemas, la gestión
+                    de equipos y la comunicación efectiva.
+                </p>
+            </div>
+        </>
+    );
 }
 
 export default About;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,159 +26,160 @@ import { motion } from "framer-motion";
 import { useRef } from "react";
 
 function App() {
-  const constraintsRef = useRef(null);
-  const constraintsRef2 = useRef(null);
+    const constraintsRef = useRef(null);
+    const constraintsRef2 = useRef(null);
 
-  Aos.init();
-  return (
-    <div className="initial">
-      <StarAnimation />
-      <div className="main_container">
-        <Navbar />
-        <main className="hero">
-          <About />
-          <div className="sub_container">
-            <h2 id="proyectos" className="sub_tittles">
-              PROYECTOS
-            </h2>
-            <ProjectArticle
-              imgProject={GymApp}
-              githubLink={"https://github.com/Locoyoccim/gym_frontend"}
-              webLink={"https://gymapp.up.railway.app/"}
-              html={html}
-              css={css}
-              js={ts}
-              reactImg={reactImg}
-              description={
-                "Aplicación web desarrollada con React y TypeScript en el frontend, Python y Django en el backend, utilizando también JWT. Esta WebApp permite registrar los entrenamientos del usuario para posteriormente visualizarlos completos o de manera individual, pensado para uso en dispositivos móviles, para mejor experiencia se recomienda usar un dispositivo Movil!."
-              }
-            />
-            <ProjectArticle
-              imgProject={countriesImg}
-              githubLink={"https://github.com/Locoyoccim/Countries_JSON"}
-              webLink={"https://main--reliable-tanuki-77c097.netlify.app"}
-              html={html}
-              css={css}
-              js={JS}
-              reactImg={reactImg}
-              direction={"reverse"}
-              description={
-                "Aplicación web desarrollada con React que permite a los usuarios explorar banderas de diferentes países del mundo. Utiliza una API externa para obtener información actualizada sobre las banderas. La interfaz es intuitiva y amigable, permitiendo a los usuarios buscar y filtrar países de manera eficiente."
-              }
-            />
-            <ProjectArticle
-              imgProject={TodoAppImg}
-              githubLink={"https://github.com/Locoyoccim/Todo-App"}
-              webLink={"https://silver-stroopwafel-e395c8.netlify.app"}
-              html={html}
-              css={css}
-              js={JS}
-              reactImg={reactImg}
-              description={
-                "Aplicación web desarrollada con React que permite a los usuarios gestionar sus tareas diarias de manera eficiente. La ToDo App proporciona una interfaz amigable para crear, editar, marcar como completadas y eliminar tareas."
-              }
-            />
-            <h2 id="tecnologias" className="sub_tittles">
-              TECNOLOGÍAS
-            </h2>
-            <motion.div className="skills_list" ref={constraintsRef}>
-              <ImgSkill
-                img={reactImg}
-                nameSkill="React"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef} // Pasar restricciones
-              />
-              <ImgSkill
-                img={css}
-                nameSkill="CSS"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef} // Pasar restricciones
-              />
-              <ImgSkill
-                img={html}
-                nameSkill="HTML"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef} // Pasar restricciones
-              />
-              <ImgSkill
-                img={JS}
-                nameSkill="JavaScript"
-                drag
-                dragConstraints={constraintsRef} // Pasar restricciones
-              />
-              <ImgSkill
-                img={python}
-                nameSkill="Python"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef} // Pasar restricciones
-              />
-              <ImgSkill
-                img={Django}
-                nameSkill="Django"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef} // Pasar restricciones
-              />
-              <ImgSkill
-                img={ts}
-                nameSkill="TypeScript"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef} // Pasar restricciones
-              />
-              <ImgSkill
-                img={sql}
-                nameSkill="SQL"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef} // Pasar restricciones
-              />
-            </motion.div>
-            <h2 className="sub_tittles">ME GUSTARÍA APRENDER!!</h2>
-            <motion.div className="To_learn" ref={constraintsRef2}>
-              <ImgSkill
-                img={Angular}
-                nameSkill="Angular"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef2} // Pasar restricciones
-              />
-              <ImgSkill
-                img={docker}
-                nameSkill="Docker"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef2} // Pasar restricciones
-              />
-              <ImgSkill
-                img={WebSocketImg}
-                nameSkill="WebSocket"
-                drag // Habilitar drag
-                dragConstraints={constraintsRef2} // Pasar restricciones
-              />
-            </motion.div>
-            <h2 id="experiencia" className="sub_tittles">
-              EXPERIENCIA
-            </h2>
-            <div className="experience_container">
-              <Experience job="COORDINADOR DE BANQUETES Y RESTAURANTE – HACIENDA EL CENTENARIO">
-                Funciones principales de lograr el éxito de los eventos de
-                acuerdo a las solicitudes, dentro del cual involucramos la
-                organización y manejo desde 1 hasta 20 meseros, teniendo una
-                gran comunicación y contacto con las áreas involucradas, durante
-                esta temporada también asumí responsabilidades de coordinador de
-                restaurante en Antigua Casona, Villa Tequila y Cafetería de
-                centro cultural.
-              </Experience>
-              <Experience job="RECEPCIONISTA – HOTEL SOLAR DE LAS ÁNIMAS">
-                Funciones principales de dar check in y check out, con el
-                sistema OPERA FULL SERVICE, generación de reservaciones, además
-                de apoyar a diferentes áreas del hotel como grupos, con la
-                creación de PM (Posting Master), además funciones como auditor
-                nocturno al cubrir vacaciones y descansos del mismo.
-              </Experience>
+    Aos.init();
+    return (
+        <div className="initial">
+            <StarAnimation />
+            <div className="main_container">
+                <Navbar />
+                <main className="hero">
+                    <About />
+                    <div className="sub_container">
+                        <h2 id="proyectos" className="sub_tittles">
+                            PROYECTOS
+                        </h2>
+                        <ProjectArticle
+                            imgProject={GymApp}
+                            githubLink={"https://github.com/Locoyoccim/gym_frontend"}
+                            webLink={"https://gymapp.up.railway.app/"}
+                            html={html}
+                            css={css}
+                            js={ts}
+                            reactImg={reactImg}
+                            description={
+                                "Aplicación web desarrollada con React y TypeScript en el frontend, Python y Django en el backend, utilizando también JWT. Esta WebApp permite registrar los entrenamientos del usuario para posteriormente visualizarlos completos o de manera individual, pensado para uso en dispositivos móviles, para mejor experiencia se recomienda usar un dispositivo Movil!."
+                            }
+                        />
+                        <ProjectArticle
+                            imgProject={countriesImg}
+                            githubLink={"https://github.com/Locoyoccim/Countries_JSON"}
+                            webLink={"https://main--reliable-tanuki-77c097.netlify.app"}
+                            html={html}
+                            css={css}
+                            js={JS}
+                            reactImg={reactImg}
+                            direction={"reverse"}
+                            description={
+                                "Aplicación web desarrollada con React que permite a los usuarios explorar banderas de diferentes países del mundo. Utiliza una API externa para obtener información actualizada sobre las banderas. La interfaz es intuitiva y amigable, permitiendo a los usuarios buscar y filtrar países de manera eficiente."
+                            }
+                        />
+                        <ProjectArticle
+                            imgProject={TodoAppImg}
+                            githubLink={"https://github.com/Locoyoccim/Todo-App"}
+                            webLink={"https://silver-stroopwafel-e395c8.netlify.app"}
+                            html={html}
+                            css={css}
+                            js={JS}
+                            reactImg={reactImg}
+                            description={
+                                "Aplicación web desarrollada con React que permite a los usuarios gestionar sus tareas diarias de manera eficiente. La ToDo App proporciona una interfaz amigable para crear, editar, marcar como completadas y eliminar tareas."
+                            }
+                        />
+                        <h2 id="tecnologias" className="sub_tittles">
+                            TECNOLOGÍAS
+                        </h2>
+                        <motion.div className="skills_list" ref={constraintsRef}>
+                            <ImgSkill
+                                img={reactImg}
+                                nameSkill="React"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={css}
+                                nameSkill="CSS"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={html}
+                                nameSkill="HTML"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={JS}
+                                nameSkill="JavaScript"
+                                drag
+                                dragConstraints={constraintsRef} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={python}
+                                nameSkill="Python"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={Django}
+                                nameSkill="Django"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={ts}
+                                nameSkill="TypeScript"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={sql}
+                                nameSkill="SQL"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef} // Pasar restricciones
+                            />
+                        </motion.div>
+                        <h2 className="sub_tittles">ME GUSTARÍA APRENDER!!</h2>
+                        <motion.div className="To_learn" ref={constraintsRef2}>
+                            <ImgSkill
+                                img={Angular}
+                                nameSkill="Angular"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef2} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={docker}
+                                nameSkill="Docker"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef2} // Pasar restricciones
+                            />
+                            <ImgSkill
+                                img={WebSocketImg}
+                                nameSkill="WebSocket"
+                                drag // Habilitar drag
+                                dragConstraints={constraintsRef2} // Pasar restricciones
+                            />
+                        </motion.div>
+                        <h2 id="experiencia" className="sub_tittles">
+                            EXPERIENCIA
+                        </h2>
+                        <div className="experience_container">
+                            <Experience job="COORDINADOR DE BANQUETES Y RESTAURANTE – HACIENDA EL CENTENARIO">
+                                Responsable de garantizar el éxito de los eventos según
+                                las solicitudes de los clientes, coordinando y
+                                supervisando equipos de entre 1 y 20 meseros, manteniendo
+                                una comunicación efectiva y constante con todas las áreas
+                                involucradas. Durante este periodo, también asumí
+                                funciones como coordinador de restaurante en Antigua
+                                Casona, Villa Tequila y la cafetería del centro cultural.
+                            </Experience>
+                            <Experience job="RECEPCIONISTA – HOTEL SOLAR DE LAS ÁNIMAS">
+                                Encargado de realizar procesos de check-in y check-out
+                                utilizando el sistema OPERA Full Service, así como de
+                                gestionar reservaciones. Brindé apoyo a distintas áreas
+                                del hotel, incluyendo el departamento de grupos. También
+                                desempeñé funciones de gerente auditor nocturno cubriendo
+                                vacaciones y días de descanso del titular.
+                            </Experience>
+                        </div>
+                    </div>
+                </main>
             </div>
-          </div>
-        </main>
-      </div>
-      <Footer />
-    </div>
-  );
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
